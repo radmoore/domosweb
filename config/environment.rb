19148157534880e697b78cfd58452ca6124bc55d
radmoore <radmoore@vebb.uni-muenster.de>
2012-07-18 16:15:21 +0200

    update to download link

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # GLOBAL VARS
 TEAM_EMAIL  = "domosaics@uni-muenster.de"
-DOWNLOAD_LOC  = "/var/www/services/angstd/download"
+DOWNLOAD_LOC  = "/var/www/services/domosaics/download"
 CURRENT_JAR   = "Jangstd-rv175.jar" 
 
 # Initialize the rails application
