7a584a3464894c5dcb8c6fa3eef199fe87feb41f
Andrew Moore <radmoore@gmail.com>
2012-07-18 16:18:27 +0200

    update to recent version of jar

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # GLOBAL VARS
 TEAM_EMAIL  = "domosaics@uni-muenster.de"
 DOWNLOAD_LOC  = "/var/www/services/domosaics/download"
-CURRENT_JAR   = "Jangstd-rv175.jar" 
+CURRENT_JAR   = "domosaics-rv179.jar" 
 
 # Initialize the rails application
 Domosaics::Application.initialize!
