a7af1e9c25c1d45ade6cd584f343f034d5acf331
Andrew Moore <radmoore@gmail.com>
2012-12-30 14:56:23 +0100

    updated files in download section

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,14 @@ require File.expand_path('../application', __FILE__)
 # GLOBAL VARS
 TEAM_EMAIL  = "domosaics@uni-muenster.de"
 DOWNLOAD_LOC  = "/var/www/services/domosaics/download"
-CURRENT_JAR   = "domosaics-rv179.jar" 
+CURRENT_JAR   = "domosaics.jar" 
 REVISION = "179"
 
 BINARIES = {
   'Windows' => 'domosaics_win.exe', 
   'Linux' => 'domosaics_linux.sh', 
-  'iOS' => 'domosaics_ios', 
-  'unknown' => 'domosaics-rv179.jar'
+  'iOS' => 'domosaics.jar', 
+  'unknown' => 'domosaics.jar'
 }
 
 # Initialize the rails application
